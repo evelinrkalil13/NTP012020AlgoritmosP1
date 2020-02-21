5fdeb0603f2069b6723adb4962e32f461f4f8f07
204 <204@academicos.cesde.edu.co>
2020-02-20 21:15:53 -0500

    Segundo commit

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,49 +1,42 @@
 var continuacion = "SI";
 var cont = 0;
-var p60 = 0;
-var p30 = 0;
-var promedio = 0;
-var promediop = 0;
+var p0 = 0;
+var m0 = 0;
+
 var encuesta = 0;
-var encuestap = 0;
-var encuestapc = 0;
 var encuestac = 0;
+var cont2 = 0
 
 while (continuacion == "SI") {
-    encuestac = parseInt(encuesta);
-    promedio = (promedio + encuestac);
-
-    encuestapc = parseInt(encuestap);
-    promediop = (promediop + encuestapc);
-
     cont++;
-    var resto = cont % 5;
+    encuestac = parseInt(encuesta);
+    
+    
+
+    var resto = cont % 10;
 
     if (resto != 0) {
 
-        encuesta = prompt("Edad persona" + cont);
-        encuestap = prompt("Peso persona" + cont);
-
+        encuesta = prompt("Ingrese el número");
+        
     } else {
         continuacion = prompt("Desea continuar? (SI/NO)");
+
+        cont2 ++;
     }
 
-    if (encuesta >= 41 && encuesta <= 59) {
-        p60 = p60 + 1;
-    } else if (encuesta >= 30 && encuesta <= 40) {
-        p30 = p30 + 1;
-    } else if (encuesta >= 60) {
-        p30 = p30 + 1;
-    }
+    if (encuesta <= 0) {
+        p0 = p0 + 1;
+    } else if (encuesta > 0) {
+        m0 = m0 + 1;
+    } 
 
 
 }
 
-console.log("El numero de personas que está entre los 40 y los 60 es: " + p60);
-console.log("El numero de personas que está entre los 30 y los 40 es: " + p30);
-console.log("El numero de personas mayores de 60 es: " + p30);
+console.log("Los numero menores o iguales a cero son: " + (p0+cont2));
+console.log("Los numero mayores a cero son: " + (m0-cont2));
 
-console.log("El numero de personas encuestadas es: " + (cont - 1));
 
-console.log("el promedio de edad es " + (promedio / (cont - 1)));
-console.log("el promedio de peso es " + (promediop / (cont - 1)));
+console.log("El numero de ... números ingresados es: " + (cont - cont2));
+
